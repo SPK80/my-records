8a4254120fd458a7e0963a67876826dd37ffd72e
SPK80 <psukonkin@gmail.com>
2022-02-26 15:26:37 +0300

    refactor: move checkBox into Record

--- src/components/records/Record.jsx
+++ src/components/records/Record.jsx
@@ -1,8 +1,16 @@
-export default function Record({ name, value }) {
+export default function Record({ name, value, onSelectionChanged }) {
 	return (
 		<div className="Record" >
+			<input
+				id={name}
+				type='checkbox'
+				onChange={(e) => {
+					onSelectionChanged(e.target.id, e.target.checked)
+					console.log(e.target);
+				}}
+			></input>
 			<span className="RecordName">{name}</span>
 			<span className="RecordValue">{value}</span>
-		</div>
+		</div >
 	)
 }
--- src/components/records/Records.jsx
+++ src/components/records/Records.jsx
@@ -5,14 +5,10 @@ export default function Records({ records, onSelectionChanged }) {
 		<div className="Records"> {
 			Object.keys(records).reverse().map(key =>
 				<div key={key}>
-					<input
-						id={key}
-						type='checkbox'
-						onChange={(e) => onSelectionChanged(e.target.id, e.target.checked)}
-					></input>
 					<Record
 						name={key}
 						value={records[key]}
+						onSelectionChanged={onSelectionChanged}
 					/>
 				</div>
 			)
