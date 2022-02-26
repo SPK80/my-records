af8c560fd99e20871db16f4115b0f7ddfb8c458f
SPK80 <psukonkin@gmail.com>
2022-02-26 15:46:31 +0300

    refactor: change record tag

--- src/components/records/Record.jsx
+++ src/components/records/Record.jsx
@@ -6,11 +6,10 @@ export default function Record({ name, value, onSelectionChanged }) {
 				type='checkbox'
 				onChange={(e) => {
 					onSelectionChanged(e.target.id, e.target.checked)
-					console.log(e.target);
 				}}
 			></input>
 			<span className="RecordName">{name}</span>
-			<span className="RecordValue">{value}</span>
+			<pre className="RecordValue">{value}</pre>
 		</div >
 	)
 }
